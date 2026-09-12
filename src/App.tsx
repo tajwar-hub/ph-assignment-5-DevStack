7b5b77300996421c657bf558fed4cdeba6c3fe58
tajwar-hub <ahmedtajwarrhythm@gmail.com>
2026-09-12 09:21:27 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-2xl text-red-900">Hello</h2>
+      <button className="btn btn-warning">Warning</button>
+    </>
+  )
+}
+
+export default App
